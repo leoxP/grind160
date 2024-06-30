f9a3550aaab0b001f447bd3606e4383f5d5abd3b
leoxP <patrabaibhab@gmail.com>
2024-06-30 23:21:58 +0530

    .

---
+++ binarytree/binartytreelevelordertraversal.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if(!root) return res;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            vector<int> lvl;
+            for(int i=0;i<size;i++){
+                TreeNode* curr=q.front();
+                q.pop();
+                lvl.push_back(curr->val);
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+            }
+            res.push_back(lvl);
+        }
+        return res;
+    }
+};
